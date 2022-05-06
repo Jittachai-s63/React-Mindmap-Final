4b147e6e9e457bb81eddd84a58b79ea15393c0b9
Jittachai-s63 <s6301012630028@email.kmutnb.ac.th>
2022-05-06 17:57:41 +0700

    add subtopic in checkbox

--- src/component/Select.js
+++ src/component/Select.js
@@ -1,41 +1,70 @@
 import pptxgen from "pptxgenjs";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Select(props) {
   let pres = new pptxgen();
   let data = JSON.parse(localStorage.getItem("selectpresent"));
   let Allnode = data.Allnode;
-  let Root = data.Root;
+  let Root = { ...data.Root };
   let Roottemp = {};
-  let selitem = [];
+  let newAllnode = {};
 
-  const [checkedState, setCheckedState] = useState(
-    new Array(Root.child.length).fill(true)
-  );
+  const [itemlist, setitemState] = useState([]);
+  const [disableState, setDisableState] = useState([]);
 
-  const [orderstate, setOrderState] = useState(
-    Array.from(Array(Root.child.length).keys())
-  );
+  const [checkedState, setCheckedState] = useState([]);
 
-  const handleOnChange = (position) => {
+  useEffect(() => {
+    const DFSitem = (Root, Allnode) => {
+      for (let i = 0; i < Root.child.length; i++) {
+        let next = Root.child[i];
+        for (let j = 0; j < Allnode.length; j++) {
+          if (next === Allnode[j].key) {
+            setitemState([...itemlist, Allnode[j].topic]);
+            itemlist.push(Allnode[j].topic);
+            DFSitem(Allnode[j], Allnode);
+          }
+        }
+      }
+    };
+    DFSitem(Root, Allnode);
+
+    setDisableState(Array(itemlist.length).fill(false));
+    setCheckedState(Array(itemlist.length).fill(true));
+  }, []);
+
+  const handleOnChange = (position, topic) => {
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
+    const updateddisState = disableState.map((item, index) => item);
+    //console.log(updateddisState);
+    let run = 0;
+    const DFSsearch = async (Root, Allnode, child) => {
+      for (let i = 0; i < Root.child.length; i++) {
+        let next = Root.child[i];
 
-    setCheckedState(updatedCheckedState);
-  };
-
-  let itemlist = [];
-  for (let i = 0; i < Root.child.length; i++) {
-    let next = Root.child[i];
-    for (let j = 0; j < Allnode.length; j++) {
-      if (next === Allnode[j].key) {
-        itemlist.push(Allnode[j].topic);
-        break;
+        for (let j = 0; j < Allnode.length; j++) {
+          if (next === Allnode[j].key) {
+            if (Allnode[j].topic == topic) {
+              child = 1;
+            } else if (child) {
+              updatedCheckedState[run] = updatedCheckedState[position];
+              updateddisState[run] = !updatedCheckedState[run];
+              child++;
+            }
+            run++;
+            DFSsearch(Allnode[j], Allnode, child);
+            if (child == 1) child--;
+          }
+        }
       }
-    }
-  }
+    };
+    DFSsearch(Root, Allnode, 0);
+    setCheckedState(updatedCheckedState);
+    setDisableState(updateddisState);
+  };
 
   const DFS = async (cur, Allnode) => {
     if (cur.child.length === 0) {
@@ -165,7 +194,7 @@ export default function Select(props) {
       align: pres.AlignH.center,
       fontSize: 30,
     });
-    DFS(Roottemp, Allnode);
+    DFS(Roottemp, newAllnode);
     let endslide = pres.addSlide();
     endslide.addText("Thank you", {
       x: 1.5,
@@ -178,27 +207,20 @@ export default function Select(props) {
     pres.writeFile({ fileName: Roottemp.topic + ".pptx" });
   };
 
-  const getselect = () => {
+  const exportsecelcslide = () => {
+    newAllnode = [];
+    Roottemp = { ...Root };
     let temp = [];
-    for (let i = 0; i < checkedState.length; i++) {
-      if (checkedState[i] == true) {
+    for (let i = 0; i < itemlist.length; i++) {
+      if (checkedState[i]) {
         for (let j = 0; j < Allnode.length; j++) {
-          if (Root.child[i] === Allnode[j].key) {
-            temp.push(Allnode[j].topic);
-            break;
+          if (itemlist[i] == Allnode[j].topic) {
+            newAllnode.push(Allnode[j]);
+            if (Root.child.indexOf(Allnode[j].key) > -1) {
+              temp.push(Allnode[j].key);
+            }
           }
         }
-      }
-    }
-    selitem = [...temp];
-  };
-
-  const exportsecelcslide = () => {
-    let temp = [];
-    Roottemp = { ...Root };
-    for (let i = 0; i < Root.child.length; i++) {
-      if (checkedState[i] == true) {
-        temp.push(Root.child[i]);
       }
     }
     Roottemp.child = temp;
@@ -206,17 +228,22 @@ export default function Select(props) {
   };
 
   const previewslide = () => {
-    let temp = [];
+    newAllnode = [];
     Roottemp = { ...Root };
-    console.log(Root);
-    for (let i = 0; i < Root.child.length; i++) {
-      if (checkedState[i] == true) {
-        temp.push(Root.child[i]);
+    let temp = [];
+    for (let i = 0; i < itemlist.length; i++) {
+      if (checkedState[i]) {
+        for (let j = 0; j < Allnode.length; j++) {
+          if (itemlist[i] == Allnode[j].topic) {
+            newAllnode.push(Allnode[j]);
+            if (Root.child.indexOf(Allnode[j].key) > -1) {
+              temp.push(Allnode[j].key);
+            }
+          }
+        }
       }
     }
     Roottemp.child = temp;
-    console.log(Root);
-    console.log(Roottemp);
   };
 
   return (
@@ -235,7 +262,8 @@ export default function Select(props) {
                     name={topic}
                     value={topic}
                     checked={checkedState[index]}
-                    onChange={() => handleOnChange(index)}
+                    disabled={disableState[index]}
+                    onChange={() => handleOnChange(index, topic)}
                   />
                   <label htmlFor={`custom-checkbox-${index}`}>{topic}</label>
                 </div>
@@ -244,7 +272,7 @@ export default function Select(props) {
           );
         })}
       </ul>
-      <div>
+      {/* <div>
         <h1>Select order export slide</h1>
         {getselect()}
         <ul>
@@ -267,7 +295,7 @@ export default function Select(props) {
             );
           })}
         </ul>
-      </div>
+      </div> */}
       <div>
         <button onClick={exportsecelcslide}>Export</button>
         <Link
@@ -277,7 +305,7 @@ export default function Select(props) {
           onClick={() =>
             localStorage.setItem(
               "present",
-              JSON.stringify({ Root: Roottemp, Allnode: Allnode })
+              JSON.stringify({ Root: Roottemp, Allnode: newAllnode })
             )
           }
         >
